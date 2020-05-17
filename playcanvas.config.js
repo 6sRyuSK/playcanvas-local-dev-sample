4bb7ac9de04f268c19e8374f80a8f352636a00d4
6sRyuSK <zikkyou19@gmail.com>
2020-05-17 18:34:07 +0900

    update playcanvas.config

--- playcanvas.config.js
+++ playcanvas.config.js
@@ -1,7 +1,7 @@
-const env = require('dotenv').config
+require('dotenv').config();
 
 module.exports = {
-  accessToken: env.accessToken,
+  accessToken: process.env.accessToken,
   scenes: ['Untitled'],
   projectId: '687266',
   branchId: '8880f68d-e54c-4253-a3bf-1dc736652d67',
